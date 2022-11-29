0d1fed241e5b63aee46ee49689f03c2017acbd47
Refael Oren <refael.oren.dev@gmail.com>
2022-11-29 21:03:39 +0200

    changings

--- src/root-cmp.vue
+++ src/root-cmp.vue
@@ -1,30 +1,26 @@
 <template>
-  <section>
-    <user-msg/>
-    <app-header />
-    <router-view/>
-  </section>
+    <section>
+        <user-msg />
+        <app-header />
+        <router-view />
+    </section>
 </template>
 
 <script>
-
-
-import { store } from './store/store'
-import appHeader from './cmps/app-header.vue'
-import userMsg from './cmps/user-msg.vue'
-import { userService } from './services/user.service'
-
+import { store } from './store/store';
+import appHeader from './cmps/app-header.vue';
+import userMsg from './cmps/user-msg.vue';
+import { userService } from './services/user.service';
 
 export default {
-
-  created() {
-    console.log('Vue App created')
-    const user = userService.getLoggedinUser()
-    if (user)  store.commit({type: 'setLoggedinUser', user})
-  },
-  components: {
-    appHeader,
-    userMsg
-  },
-}
+    created() {
+        console.log('Vue App created');
+        const user = userService.getLoggedinUser();
+        if (user) store.commit({ type: 'setLoggedinUser', user });
+    },
+    components: {
+        appHeader,
+        userMsg,
+    },
+};
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,28 +1,23 @@
 <template>
-  <section class="container home text-center">
-    <h1>Home Page</h1>
-    <h5 class="text-center">
-      Powered up Frontend 
-    </h5>
-    <img alt="Vue logo" src="../assets/logo.png">
-  </section>
-
-  
+    <section class="container home text-center">
+        <h1>Home Page</h1>
+        <img
+            src="https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg"
+            alt=""
+        />
+        <h5 class="text-center">Powered up Frontend</h5>
+        <img alt="Vue logo" src="../assets/logo.png" />
+    </section>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data() {
-    return {
-    }
-  },
-  computed: {
-  },
-  created() {
-  },
-  methods: {
-  }
-  
-}
+    name: 'home',
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    methods: {},
+};
 </script>
