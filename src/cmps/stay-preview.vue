32805bbfb568332d2f5fef6f4f2b43997ad8f5ac
Efrat Stoztki <efratvaik@gmail.com>
2022-11-30 13:42:04 +0200

    version 0.6

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -20,14 +20,16 @@
                 </div>
 
             </router-link>
-                <carousel>
+
+                <!-- <carousel>
                     <slide>
                         Slide 1 Content
                     </slide>
                     <slide>
                         Slide 2 Content
                     </slide>
-                </carousel>
+                </carousel> -->
+
         </section>
     </section>
 </template>
