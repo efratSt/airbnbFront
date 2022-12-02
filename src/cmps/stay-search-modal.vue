d13d8c7a60faba54e715c80742bfe0c4baad01d9
Refael Oren <refael.oren.dev@gmail.com>
2022-12-02 10:23:21 +0200

    search setup

--- src/cmps/stay-search-modal.vue
+++ src/cmps/stay-search-modal.vue
@@ -2,28 +2,56 @@
     <section class="search-modal">
         <div class="search-container">
             <div class="search-line">
-                <label class="search-line-btn">
+                <div class="where search-line-btn">
                     <p>Where</p>
-
                     <input type="text" placeholder="Search destination " />
-                </label>
-                <div class="check-in search-line-btn">
-                    <p>Check in</p>
-                    <span>Add dates</span>
                 </div>
-                <div class="check-out search-line-btn">
+                <div @click="calOpen" class="check-in search-line-btn">
+                    <p>Check in</p>
+                    <span>{{ date(range.start) }}</span>
+                </div>
+                <div @click="calOpen" class="check-out search-line-btn">
                     <p>Check out</p>
-                    <span>Add dates</span>
+                    <span>{{ date(range.end) }}</span>
                 </div>
                 <div class="who search-line-btn">
                     <p>Who</p>
                     <span>Add guests</span>
                 </div>
             </div>
+            <Date-picker
+                class="calender"
+                v-if="isCalOpen"
+                v-model="range"
+                is-range
+                :columns="2"
+                color="gray"
+            />
         </div>
     </section>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            range: {
+                start: null,
+                end: null,
+            },
+
+            isCalOpen: false,
+        };
+    },
+
+    methods: {
+        date(date) {
+            return date ? new Date(date).toLocaleDateString() : 'Add dates';
+        },
+
+        calOpen() {
+            this.isCalOpen = !this.isCalOpen;
+        },
+    },
+};
 </script>
