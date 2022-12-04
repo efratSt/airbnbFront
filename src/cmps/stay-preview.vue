fd8c0a5d7b8e402d52c63caf35474b9f34bfd37c
Nirosss <aniroiter@gmail.com>
2022-12-04 22:26:29 +0900

    fixed spacing in amenities list

--- src/cmps/stay-preview.vue
+++ src/cmps/stay-preview.vue
@@ -19,24 +19,18 @@
                 </button>
 
                 <div class="text-container">
-
                     <div class="title"><span>{{ stay.name }}</span>
                         <div class="rating"><span class="star">★ </span>{{ rateCalc }}</div>
                     </div>
-
                     <div>
                         <p class="added">Added {{ dateCalc }} ago</p>
                     </div>
-
                     <p class="price-night"><span class="price">{{ currencyCode }} {{ stay.price }} </span> &nbsp/&nbsp
                         night</p>
                 </div>
-
             </section>
         </li>
     </section>
-
-
 </template>
 
 <script>
