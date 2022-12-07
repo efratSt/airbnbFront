235bfe94283da004c7c93de55cbdb1854d53b69b
Nirosss <aniroiter@gmail.com>
2022-12-08 02:21:19 +0900

    fixed userimgs

--- src/views/stay-app.vue
+++ src/views/stay-app.vue
@@ -1,46 +1,46 @@
 <template>
-    <section class="stay-app">
-        <!-- <stay-labels-filter/> -->
-        <stay-list :stays="getStays" />
-    </section>
-    <!-- <pre>{{stays}}</pre> -->
+  <section class="stay-app">
+    <!-- <stay-labels-filter/> -->
+    <stay-list :stays="getStays" />
+  </section>
+  <!-- <pre>{{stays}}</pre> -->
 </template>
 
 <script>
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service';
-import { stayService } from '../services/stay.service.local';
-import stayList from '../cmps/stay-list.vue';
-// import stayLabelsFilter from '../cmps/stay-labels-filter.vue';
+  import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+  import { stayService } from '../services/stay.service.local'
+  import stayList from '../cmps/stay-list.vue'
+  // import stayLabelsFilter from '../cmps/stay-labels-filter.vue'
 
-export default {
+  export default {
     data() {
-        return {
-            stayToAdd: stayService.getEmptyStay(),
-            // isExplore: false
-        };
+      return {
+        stayToAdd: stayService.getEmptyStay(),
+        // isExplore: false
+      }
     },
     computed: {
-        loggedInUser() {
-            return this.$store.getters.loggedinUser;
-        },
-        getStays() {
-            return this.$store.getters.stays;
-        },
-        isExplore() {
-            return this.$route.name === 'stay-explore';
-        },
+      loggedInUser() {
+        return this.$store.getters.loggedinUser
+      },
+      getStays() {
+        return this.$store.getters.stays
+      },
+      isExplore() {
+        return this.$route.name === 'stay-explore'
+      },
     },
     created() {
-        this.$store.dispatch({ type: 'loadStays' });
-        this.$store.dispatch({ type: 'loadOrders' });
-        this.$store.commit({ type: 'setFilterBy', filterBy: null })
-        this.$store.commit({ type: 'setFilterByLabel', filterLabel: "" })
+      this.$store.dispatch({ type: 'loadStays' })
+      this.$store.dispatch({ type: 'loadOrders' })
+      this.$store.commit({ type: 'setFilterBy', filterBy: null })
+      this.$store.commit({ type: 'setFilterByLabel', filterLabel: '' })
     },
 
     methods: {},
     components: {
-        stayList,
-        // stayLabelsFilter,
+      stayList,
+      // stayLabelsFilter,
     },
-};
+  }
 </script>
