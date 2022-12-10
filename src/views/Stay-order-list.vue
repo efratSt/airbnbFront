4ca0a055903a0c741e7c91ab64e2e41e04375bae
Efrat Stoztki <efratvaik@gmail.com>
2022-12-10 21:52:21 +0200

    ordert

--- src/views/Stay-order-list.vue
+++ src/views/Stay-order-list.vue
@@ -9,7 +9,7 @@
                 <div class="order-list-date-start">Start Date</div>
                 <div class="order-list-date-end">End Date</div>
                 <div class="order-list-date-create">Created</div>
-                <div class="order-list-place-status">Status</div>
+                <div class="order-list-place-status">tatus</div>
             </div>
             <div class="order-list-container" v-for="order in ordersByUser" key="ordersByUser._id">
                 <div class="order-list-img">
